e288dbfaa1d36fd2b04246d1e17260a7da38cf3e
biomagic <2755527@mail.ru>
2016-10-05 09:40:54 +0300

    hw3-4 replace function in object start

--- js-3-4/js/script.js
+++ js-3-4/js/script.js
@@ -2,31 +2,33 @@ function documentCreate() {
 
   // Header form
 
-  var testHeader = '<header class="test-header"><p>Тест по программированию</p></header>';
+  var testHeader = '<header class="test-header"><h2>Тест по программированию</h2></header>';
 
-  // Count of li elements
+  // Call li items
 
   var variantText = 'Вариант ответа №';
 
   var variantAnswerItem = '';
 
-  var input = '<input type="checkbox" class="checkbox"';
+  var input = '<input type="checkbox" class="main__checkbox" id="a';
+
+  var button = '<button class="review btn-lg btn-success">Проверить мои результаты</button>'
 
   for (var i = 1; i <= 3; i++) {    
-    variantAnswerItem += '<li class="main__li">' + input + '<p>' + variantText + i + '</p>' + '</li>';
+    variantAnswerItem += '<li class="main__li">' + input + i + '">' + '<p class="main__p">' + variantText + i + '</p>' + '</li>';
   }
 
-  // Count of ul elements
+  // Call ul
 
   var questionText = '. Вопрос №';
 
 	var questionsList = '';
 
 	for (var i = 1; i <= 3; i++) {
-		questionsList += '<p>' + i + questionText + i + '</p>' + '<ul class="main__ul">' + variantAnswerItem + '</ul>';
+		questionsList += '<h3>' + i + questionText + i + '</h3>' + '<ul class="main__ul">' + variantAnswerItem + '</ul>';
 	}
 
-  var result = '<div class="wrapper">' + testHeader + '<main>' + questionsList + '</main>' + '</div>';
+  var result = '<div class="wrapper">' + testHeader + '<main class="main">' + questionsList + '</main>' + button + '</div>';
 
   return result;
 }
