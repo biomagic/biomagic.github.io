1eb86c11a846d337acff4ac19c223f59e2cf22f2
biomagic <2755527@mail.ru>
2016-10-07 17:51:45 +0300

    js 7-8, titles finished

--- js-7-8/js/script.js
+++ js-7-8/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+// View selected tab
+
 var $elements = $("input[name='tabs'] + label");
 var $tab = $('.tab');;
 
@@ -11,5 +13,33 @@ $elements.on('click', function() {
 		}
 	}
 });
+
+// View tooltips
+
+var $inputs = $(".input-box__input");
+var $pEl;
+var $title;
+var $button = $(".input-box__button");
+
+$inputs.on('mouseover', function() {
+	$title = $(this).attr("title");
+	$(this).attr("title", "");	
+	$pEl = $(this).next();
+	$pEl.text($title);
+})
+
+$inputs.on('mouseleave', function() {
+	$pEl.empty();
+	$(this).attr("title", $title);
+})
+
+$button.on('click', function() {
+	for(var i in $inputs) {
+		$title = $($inputs[i]).attr("title");
+		$pEl = $($inputs[i]).next();
+		$pEl.text($title);
+	}
+})
+
 
 });
