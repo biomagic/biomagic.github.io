af92ab569a50cea1f969d86553cedd3b5319c847
biomagic <2755527@mail.ru>
2016-10-28 18:09:38 +0300

    hw js 9-10 finished
    hw js 9-10 finished
    
    hw js 9-10 finished

--- js-9-10/js/script.js
+++ js-9-10/js/script.js
@@ -18,8 +18,40 @@ $(function() {
 		}
 	});
 
-// Checkboxes
+// Dropdown
 
+$('.dropdown').append('<span class="caret"></span>');
+
+$('.dropdown').hover(function() {
+
+	$(this).children('.sub-menu').show();
+
+	var $liHeight = $(this).children('ul').contents('li').length * parseInt($('.sub-menu > li').css('height'));
+
+	$(this).children('.sub-menu').css({
+		height: 0
+	});
+
+	$(this).removeClass('caret-right');
+	
+	$(this).children('.sub-menu').animate({
+	  height:  $liHeight + 'px'
+	}, 150);
+
+
+	}, function() {
+	
+	$(this).addClass('caret-right');
+	
+	$(this).children('.sub-menu').animate({
+	  height: '0'
+	}, 150);
+
+	$(this).children('.sub-menu').css({
+		overflow: 'hidden'
+	});
+
+	});
 
 
 });
